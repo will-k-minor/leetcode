e35d07c72f6288f9931e536b1f9a3785b7a9cf1d
William Minor <willkminor@gmail.com>
2024-06-19 20:32:05 -0500

    problem 5: longest palindromic substring

---
+++ longestPalindromicSub.go
@@ -0,0 +1,33 @@
+package main
+
+func longestPalindrome(s string) string {
+	isPalindrome := func(sub string) bool {
+		i := 0
+		k := len(sub) - 1
+
+		for i <= k {
+			if sub[i] != sub[k] {
+				return false
+			}
+			i++
+			k--
+		}
+		return true
+	}
+
+	if isPalindrome(s) {
+		return s
+	}
+
+	for window := len(s) - 1; window > 0; window-- {
+		// Check each substring of the current window size
+		for i := 0; i+window <= len(s); i++ {
+			wordWindow := s[i : i+window]
+			if isPalindrome(wordWindow) {
+				return wordWindow
+			}
+		}
+	}
+
+	return string(s[0])
+}
--- runner.go
+++ runner.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	result := twoSum([]int{2, 7, 11, 15}, 9)
+	result1 := twoSum([]int{2, 7, 11, 15}, 9)
+	fmt.Printf("Results: %v", result1)
 
-	fmt.Printf("Results: %v", result)
+	result5 := longestPalindrome("babad")
+	fmt.Printf("Longest Palindrom Substring is: %v", result5)
 }
