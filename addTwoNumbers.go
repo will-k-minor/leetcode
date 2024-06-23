082899b47ef6b2bc6f822c4244b97f8df91d5c6c
William Minor <willkminor@gmail.com>
2024-06-22 21:31:59 -0500

    Problem 11: Container with the most water

--- addTwoNumbers.go
+++ addTwoNumbers.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "math"
-    "strconv"
+	"math"
+	"strconv"
 )
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -12,33 +12,36 @@ import (
  *     Next *ListNode
  * }
  */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    num1 :=float64(0);
-    num2 := float64(0);
-    var exp = math.Pow(10, 2);
+	num1 := float64(0)
+	num2 := float64(0)
 
-    for i := float64(0); l1 != nil; i++ {
-        num1 = num1 + float64(l1.Val)*math.Pow(10, i) / exp
-        // fmt.Printf("num1 %f", num1)
-        l1 = l1.Next
-    }
+	for i := float64(0); l1 != nil; i++ {
+		num1 = num1 + float64(l1.Val)*math.Pow(10, i)
+		// fmt.Printf("num1 %f", num1)
+		l1 = l1.Next
+	}
 
-    for i := float64(0); l2 != nil; i++ {
-        // fmt.Printf("%v", l2.Val)
-        num2 = num2 + float64(l2.Val)*math.Pow(10, i) / exp
-        l2 = l2.Next
-    }
-    sumString := strconv.Itoa(int((num1 + num2) * exp));
-    result := &ListNode{}
-    head := result;
-    fmt.Printf("%v", sumString)
-    for i := len(sumString) - 1; i >=0 ; i-- {
-        val, _ := strconv.Atoi(string(sumString[i]))
-        result.Next = &ListNode{Val: val}
-        result = result.Next;
-    }
+	for i := float64(0); l2 != nil; i++ {
+		// fmt.Printf("%v", l2.Val)
+		num2 = num2 + float64(l2.Val)*math.Pow(10, i)
+		l2 = l2.Next
+	}
+	sumString := strconv.Itoa(int((num1 + num2)))
+	result := &ListNode{}
+	head := result
+	// fmt.Printf("%v", sumString)
+	for i := len(sumString) - 1; i >= 0; i-- {
+		val, _ := strconv.Atoi(string(sumString[i]))
+		result.Next = &ListNode{Val: val}
+		result = result.Next
+	}
 
-    //fmt.Printf("%v", sumString);
-
-    return head.Next;
+	return head.Next
 }
