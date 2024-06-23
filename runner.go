58c1599fa8e06e8fc1bf272493a23cb39049a505
William Minor <willkminor@gmail.com>
2024-06-22 22:22:28 -0500

    whoops

--- runner.go
+++ runner.go
@@ -7,9 +7,11 @@ func main() {
 	fmt.Printf("Results: %v \n", result1)
 
 	result5 := longestPalindrome("babad")
-	fmt.Printf("Longest Palindrom Substring is: %v \n", result5)
+	fmt.Printf("Longest Palindrom Substring is: %v", result5)
 
-	const problem6String = "PAYPALISHIRING";
-	result6 := zigzag(problem6String, 3);
-	fmt.Printf("ZigZag with %v number of rows: %v \n", problem6String, result6);
+	result11 := ContainerWithMostWater([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})
+	fmt.Printf("Container with most water contains %v area", result11)
+
+	result12 := IntegerToRoman(1994)
+	fmt.Printf("%v is %v", 1994, result12)
 }
