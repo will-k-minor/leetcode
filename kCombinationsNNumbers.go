4f3423d7fa73f4aae4f6da95ef89531fa76bd664
will-k-minor <willkminor@gmail.com>
2024-07-19 23:02:15 -0500

    Problem 77: Give the number of combinations of k length from numbers 1 -> n

---
+++ kCombinationsNNumbers.go
@@ -0,0 +1,48 @@
+package main
+
+func combine(n int, k int) [][]int {
+	/**
+	  there is less and less options as we reach the end
+	  func backtrack(list)
+
+	      if we reach a list of size k, store that list in the result
+
+	      otherwise
+
+	      loop through the range (1 -> n)
+	          run current list through this function i.e [1, 1+currentIndex]
+	      -----------------------
+	      second iteration [1 ,2]
+	      see if this is size k
+	          if so, store it in results
+	      else
+	          loop through 2 -> n
+	              run [1, 2, 2+currentIndex] through the next iteration
+	      ----
+	      add infinitum
+	*/
+	result := [][]int{}
+
+	var dfsCombo func(cur []int)
+	dfsCombo = func(cur []int) {
+		if len(cur) == k {
+			result = append(result, cur)
+		} else {
+			idx := -1
+			if len(cur)-1 >= 0 {
+				idx = cur[len(cur)-1]
+			} else {
+				idx = 0
+			}
+			idx++
+
+			for idx <= n {
+				dfsCombo(append(cur, idx))
+				idx++
+			}
+		}
+	}
+
+	dfsCombo([]int{})
+	return result
+}
